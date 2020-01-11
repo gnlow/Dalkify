12bb0d70d7944081b31ea3c6ca80ff206fc8730f
Gnlow <entrydark@gmail.com>
2020-01-11 16:36:58 +0900

    Add tab permission

--- background.js
+++ background.js
@@ -1,19 +1,22 @@
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     if (changeInfo.status == "complete") {
-        var url = new URL(tab.url);
-        if (url.pathname.length > 4 || url.pathname == "/ws") {
-            chrome.tabs.executeScript(
-                null, {
-                    file: "dalkify.js"
-                },
-                () => {
-                    chrome.tabs.executeScript(
-                        null, {
-                            code: `console.log('${JSON.stringify(changeInfo)}');`
-                        }
-                    )
-                }
-            );
+        if (tab.url) {
+            var url = new URL(tab.url);
+            if (url.pathname.length > 4 || url.pathname == "/ws") {
+                chrome.tabs.executeScript(
+                    null, {
+                        file: "dalkify.js"
+                    },
+                    () => {
+                        chrome.tabs.executeScript(
+                            null, {
+                                code: `console.log('${JSON.stringify(changeInfo)}');`
+                            }
+                        )
+                    }
+                );
+            }
         }
+
     }
 });
